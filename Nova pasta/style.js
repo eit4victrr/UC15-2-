e77832758050aedf955a1e8382f51b72525ce81e
Brunão do Califórnia <victor1411barros@gmail.com>
2026-07-30 17:56:28 -0300

    Add files via upload

---
+++ Nova pasta/style.js
@@ -0,0 +1,43 @@
+let nome = ' Pedro';
+let curso = 'MedTec';
+let ano = 2;
+
+console.log("Bem Vindo! " + nome + "Você é do curso " + curso)
+
+/*let texto = "Bem Vindo! " + nome + "Você é do curso " + curso + "da série do " + ano + "º" */
+
+/*let texto = `Bem vindo ${nome}! você é do ${curso} e da série ${ano}`*/
+
+console.log(texto)
+
+let multilinhas = `
+Primeira linha
+Segunda linha
+Terceira linha 
+`;
+
+console.log(multilinhas)
+
+let texto = ' Desenvolvedor Front-end ';
+
+// Comprimento
+
+let tamanho = tamanho.length
+
+// Remover espaços das bordas
+let remove = texto.trim()
+
+console.log (remove)
+
+// Maiúsculo e minúsculo
+
+texto.trim().toUpperCase()
+texto.trim().toLowerCase()
+
+console.log(maiusculo)
+console.log(minusculo)
+
+let mudanca = texto.replace('Front-end', 'Back-end')
+
+console.log(mudanca)
+console.log(tamanho)
