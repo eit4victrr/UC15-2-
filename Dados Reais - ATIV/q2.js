339520bfead7bdb8e45234ac9501e4654042b0c9
Brunão do Califórnia <victor1411barros@gmail.com>
2026-07-31 18:12:50 -0300

    Add files via upload

---
+++ Dados Reais - ATIV/q2.js
@@ -0,0 +1,23 @@
+let produto = prompt("Escreva o nome do produto:")
+console.log("Produto: " + produto)
+
+let preco = Number(prompt("Escreva o preço original do produto:"))
+console.log("Preço original: R$ " + preco)
+
+let desconto = Number(prompt("Escreva o percentual de desconto:"))
+console.log("Desconto: " + desconto + "%")
+
+let valorDesconto = preco * (desconto / 100)
+let precoFinal = preco - valorDesconto
+
+console.log("Resultado")
+console.log("Produto: " + produto)
+console.log("Valor do desconto: R$ " + valorDesconto)
+console.log("Preço final: R$ " + precoFinal)
+
+alert(
+    "Produto:  " + produto +
+    "Valor do desconto: R$ " + valorDesconto +
+    "Preço final: R$ " + precoFinal 
+)
+
