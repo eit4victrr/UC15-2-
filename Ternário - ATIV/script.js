e70c77513eae28225203c14ef97f60977e35f84d
Brunão do Califórnia <victor1411barros@gmail.com>
2026-08-20 17:52:16 -0300

    Add files via upload

---
+++ Ternário - ATIV/script.js
@@ -0,0 +1,44 @@
+// 1.
+
+let descricao;
+
+if (temperatura > 30) {
+
+  descricao = 'Quente';
+
+} else {
+
+  descricao = 'Agradável';
+
+}
+
+
+// 2.
+
+let tipo;
+
+if (numero % 2 === 0) {
+
+  tipo = 'par';
+
+} else {
+
+  tipo = 'ímpar';
+
+}
+
+
+// 3.
+
+let saudacao;
+
+if (hora < 12) {
+
+  saudacao = 'Bom dia';
+
+} else {
+
+  saudacao = 'Boa tarde/noite';
+
+}
+
